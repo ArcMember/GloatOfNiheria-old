674e6ed645261fbd7fea7e713c41bfd669a94485
ArchInglor <107791080+ArcMember@users.noreply.github.com>
2023-03-03 03:43:01 +0300

    Release 1.3
    * Map remade and optimized
    * Instruments section remade

--- docs/menu/tools/map/map.js
+++ docs/menu/tools/map/map.js
@@ -1,30 +1,35 @@
 function makeMap(type, showLocations=false) {
-    let mapPic = "map-geographical.png"
+    let mapUrl = "map-geographical"
     switch(type) {
         case "geographical":
-            mapPic = "map-geographical.png";
+            mapUrl = "map-geographical"
             break;
         case "political": 
-            mapPic = "map-political.png";
+            mapUrl = "map-political"
             break;
         case "extended": 
-            mapPic = "map-extended.png";
+            mapUrl = "map-extended"
             break;
     }
 
     let regions = L.layerGroup()
     let marks = L.layerGroup()
+    var bounds = [[0, 0], [-256, 256]];
 
     var map = L.map('map', {
-        crs: L.CRS.Simple,
-        minZoom: -1,
-        maxZoom: 3,
+        minZoom: 1,
+        maxZoom: 4,
+        zoom: 1,
+        center: [-128, 128],
+        trackResize: true,
+        maxBounds: bounds,
         layers: [regions, marks],
+        crs: L.CRS.Simple,
     });
-    var bounds = [[0,0], [1000,1000]];
-    var image = L.imageOverlay(mapPic, bounds).addTo(map);
-    map.fitBounds(bounds);
-    map.setView([500,500], 0)
+    L.tileLayer(mapUrl+'/{z}/{x}/{y}.jpg', {
+        tileSize: 128,
+        noWrap: true,
+    }).addTo(map);
 
     if (L.Browser.ielt9) {
         alert('Обновите браузер.');
@@ -43,10 +48,18 @@ function makeMap(type, showLocations=false) {
     }
     function onMapClick(e) {
         if (window.localStorage.getItem("admin") === "true") {
-            navigator.clipboard.writeText(`[${e.latlng.lat}, ${e.latlng.lng}]`);
+            document.getElementById("debug").innerHTML += `[${e.latlng.lat.toFixed(2)}, ${e.latlng.lng.toFixed(2)}], `
+            navigator.clipboard.writeText(document.getElementById("debug").innerHTML);
         }
     }
-    
+
+    document.addEventListener("keydown", (e) => {
+        if (e.keyCode == 68 && e.shiftKey && e.altKey) {
+            document.getElementById("debug").innerHTML = ``
+        }
+    // do something
+    });
+
     function makeLocations(map) {
         // Locations are loaded from the "locations.js" file
         for (const loc of locations) {
@@ -57,10 +70,7 @@ function makeMap(type, showLocations=false) {
                     fillOpacity: 0.2,
                     opacity: 0.7,
                 }).addTo(map);
-                polygon.bindPopup(`<div map-popup><b>${loc['name']}</b><br><i><a target=”_blank” href="${loc['url']}">Нажмите сюда, чтобы перейти.</a></i></div>`);
-                polygon.on('click', function(e) { 
-                    polygon.openPopup();
-                })
+                /* polygon.bindPopup(`<div map-popup><b>${loc['name']}</b><br><i><a target=”_blank” href="${loc['url']}">Нажмите сюда, чтобы перейти.</a></i></div>`); */
                 regions.addLayer(polygon)
             }
         }
@@ -70,12 +80,9 @@ function makeMap(type, showLocations=false) {
                     color: loc['color'],
                     fillColor: loc['color'],
                     fillOpacity: 0.5,
-                    radius: 15
+                    radius: 3
                 }).addTo(map);
-                circle.bindPopup(`<div map-popup><b>${loc['name']}</b><br><i><a target=”_blank” href="${loc['url']}">Нажмите сюда, чтобы перейти.</a></i></div>`);
-                circle.on('click', function(e) { 
-                    circle.openPopup();
-                })
+                /* circle.bindPopup(`<div map-popup><b>${loc['name']}</b><br><i><a target=”_blank” href="${loc['url']}">Нажмите сюда, чтобы перейти.</a></i></div>`); */
                 marks.addLayer(circle)
             }
         }
@@ -85,15 +92,42 @@ function makeMap(type, showLocations=false) {
                     color: loc['color'],
                     fillColor: loc['color'],
                     fillOpacity: 0.5,
-                    radius: 5
+                    radius: 1
                 }).addTo(map);
-                circle.bindPopup(`<div map-popup><b>${loc['name']}</b><br><i><a target=”_blank” href="${loc['url']}">Нажмите сюда, чтобы перейти.</a></i></div`);
-                circle.on('click', function(e) { 
-                    circle.openPopup();
-                })
+                /* circle.bindPopup(`<div map-popup><b>${loc['name']}</b><br><i><a target=”_blank” href="${loc['url']}">Нажмите сюда, чтобы перейти.</a></i></div`); */
                 marks.addLayer(circle)
             }
         }
     }
 }
 
+class Vector {
+    constructor(x, y) {
+        this.x = x;
+        this.y = y;
+    }
+    plus(number) {
+        var x = this.x + number;
+        var y = this.y + number;
+        return new Vector(x, y);
+    }
+    minus(number) {
+        var x = this.x - number;
+        var y = this.y - number;
+        return new Vector(x, y);
+    }
+    multiply(number) {
+        var x = this.x * number;
+        var y = this.y * number;
+        return new Vector(x, y);
+    }
+    divide(number) {
+        var x = this.x / number;
+        var y = this.y / number;
+        return new Vector(x, y);
+    }
+    toString() {
+        return 'Vector { x: ' + this.x + ', y: ' + this.y + ' }';
+    }
+    
+}
